7a4e141736540f630000afd3cb26e982f8396b97
Hunter Belanger <hunter.belanger@gmail.com>
2021-04-30 17:52:35 +0200

    Fixes typo in install_essentials

--- src/install_essentials.sh
+++ src/install_essentials.sh
@@ -2,9 +2,9 @@ install_essentials_fedora() {
   # Good for Fedora 30 or later, this installs all dependencies for my own
   # development, and also things I need to build i3-gaps, and use i3 and such.
   sudo dnf install cmake gcc make python-devel git ctags vim alacritty zsh \
-    xbacklight feh xorg-x11-drv-libinput xorg-x11-server-utils ibxcb-devel \
+    xbacklight feh xorg-x11-drv-libinput xorg-x11-server-utils libxcb-devel \
     xcb-util-keysyms-devel xcb-util-devel xcb-util-wm-devel xcb-util-xrm-devel \
     yajl-devel libXrandr-devel startup-notification-devel libev-devel \
     xcb-util-cursor-devel libXinerama-devel libxkbcommon-devel \
-    libxkbcommon-x11-devel pcre-devel pango-devel automake nmcli zip unzip
+    libxkbcommon-x11-devel pcre-devel pango-devel automake zip unzip
 }
