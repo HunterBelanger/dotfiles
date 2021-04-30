0eaca471b07bf4330f175b2cfc2c490c66f6222e
Hunter Belanger <hunter.belanger@gmail.com>
2021-04-30 18:02:25 +0200

    Adds packages to install_essentials

--- src/install_essentials.sh
+++ src/install_essentials.sh
@@ -5,6 +5,6 @@ install_essentials_fedora() {
     xbacklight feh xorg-x11-drv-libinput xorg-x11-server-utils libxcb-devel \
     xcb-util-keysyms-devel xcb-util-devel xcb-util-wm-devel xcb-util-xrm-devel \
     yajl-devel libXrandr-devel startup-notification-devel libev-devel \
-    xcb-util-cursor-devel libXinerama-devel libxkbcommon-devel \
+    xcb-util-cursor-devel libXinerama-devel libxkbcommon-devel g++ gfortran \
     libxkbcommon-x11-devel pcre-devel pango-devel automake zip unzip
 }
